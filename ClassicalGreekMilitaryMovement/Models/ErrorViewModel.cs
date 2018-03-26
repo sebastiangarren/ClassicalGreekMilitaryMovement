dbf18679e7aa383bfab3eff34bb8fc85dbbedfd6
SebastianG <sebastian.garren@gmail.com>
2018-03-25 20:49:21 -0500

    Add project files.

---
+++ ClassicalGreekMilitaryMovement/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ClassicalGreekMilitaryMovement.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
