4d0d8259abf719c71022cd8f5f2a461bb7ee9ec1
SebastianG <sebastian.garren@gmail.com>
2018-04-15 16:29:25 -0500

    Leaflet and GoogleMaps Libraries implemented

---
+++ ClassicalGreekMilitaryMovement/wwwroot/js/JavaScript.js
@@ -0,0 +1,8 @@
+﻿var mymap = L.map('mapid').setView([37.805, 23.00], 8);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1Ijoic2ViYXN0aWFuZ2FycmVuIiwiYSI6ImNqZnJoa2s2OTJramkzMG52ODMxNXFvaHYifQ.qFHF71NGXq17Mrud8YnOSA'
+}).addTo(mymap);
